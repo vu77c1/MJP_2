1095bab8023315f31a8da51f95cd21ef3c98b2fc
Vu77c1 <vu77c1@gmail.com>
2023-11-24 14:08:20 +0700

    1

--- src/main/java/Common/JdbcConfig.java
+++ src/main/java/Common/JdbcConfig.java
@@ -1,2 +1,12 @@
-package Common;public class JdbcConfig {
+package Common;
+
+import java.sql.Connection;
+
+public class JdbcConfig {
+    public static Connection connection;
+    public static String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    public static String JDBC_URL = "jdbc:sqlserver://localhost:1433;database=IJP;TrustServerCertificate=true;";
+    public static String USERNAME = "sa";
+    public static String PASSWORD = "Tuanvu1993@";
+
 }
--- src/main/java/MainManager.java
+++ src/main/java/MainManager.java
@@ -1,2 +1,116 @@
-package PACKAGE_NAME;public class MainManager {
+import java.util.Scanner;
+
+public class MainManager {
+    private static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n;
+        do {
+            n = menu(sc);
+            productManagement(n, sc);
+
+        } while (!"0".equalsIgnoreCase(String.valueOf(n)));
+
+        sc.close();
+
+    }
+    public static int menu(Scanner sc) {
+        int n;
+
+        do {
+            System.out.println("----------------MANAGEMENT----------------");
+            System.out.println();
+
+            System.out.println();
+            System.out.println("            0. Exit");
+            System.out.println("            1. Quản lý hộ dân");
+            System.out.println("            2. Quản lý cán bộ");
+            System.out.println("            3. Quản lý người đại diện");
+            System.out.println("            4. Quản lý công ty ủng hộ");
+            System.out.println("            5. Quản lý Ủy Ban");
+            System.out.println("            6. Quản lý phân phối");
+            System.out.println("            7. Quản lý chi tiết phân phối");
+            System.out.println("            8. Quản lý đối tượng ưu tiên");
+            System.out.println("            9. Quản lý công dân");
+            System.out.println("            10. Quản lý đối tượng công dân");
+            System.out.println("            11. Thống kê");
+            System.out.println();
+            System.out.println("            What do you want to choose?");
+
+
+            int m = -1;
+
+            do {
+                try {
+                    System.out.print("Please choose....");
+                    m = Integer.parseInt(sc.nextLine());
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input. Please enter a valid integer.");
+                }
+            } while (m == -1);
+
+            n = m;
+
+
+        } while (!(n >= 0 && n <= 11));
+        return n;
+    }
+
+    public static int productManagement(int n, Scanner sc) {
+        try {
+
+            switch (n) {
+                case 1:
+                    System.out.println("Quản lý hộ dân");
+                    break;
+                case 2:
+                    System.out.println("Quản lý cán bộ");
+                    break;
+                case 3:
+                    System.out.println("Quản lý người đại diện");
+
+                    break;
+                case 4:
+                    System.out.println("Quản lý công ty ủng hộ");
+
+                    break;
+                case 5:
+                    System.out.println("Quản lý Ủy Ban");
+                    break;
+                case 6:
+                    System.out.println("Quản lý phân phối");
+
+                    break;
+                case 7:
+                    System.out.println("Quản lý chi tiết phân phối");
+
+                    break;
+                case 8:
+                    System.out.println("Quản lý đối tượng ưu tiên");
+
+
+                    break;
+                case 9:
+                    System.out.println("Quản lý công dân");
+
+                    break;
+                case 10:
+                    System.out.println("Quản lý đối tượng công dân");
+
+                    break;
+                case 11:
+                    System.out.println("Thống kê");
+
+                    break;
+                case 0:
+                    System.out.println("Close program.....");
+                    break;
+
+            }
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+        return n;
+
+    }
 }
