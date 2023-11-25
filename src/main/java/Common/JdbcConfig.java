19aa4e6909de39b53110af157ab5ff9ef657dae3
Dung <hohuydung20399@gmail.com>
2023-11-25 22:45:40 +0700

    v2

--- src/main/java/Common/JdbcConfig.java
+++ src/main/java/Common/JdbcConfig.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 public class JdbcConfig {
     public static Connection connection;
     public static String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static String JDBC_URL = "jdbc:sqlserver://localhost:1433;database=miniproject_english;TrustServerCertificate=true;";
+    public static String JDBC_URL = "jdbc:sqlserver://MSI\\SQLEXPRESS:1433;database=miniproject_english;TrustServerCertificate=true;";
     public static String USERNAME = "sa";
     public static String PASSWORD = "123456789";
 }
