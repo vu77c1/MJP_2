1abd01c113a1ea99039b930949ab8a951cb44773
vu77c1 <vu77c1@gmail.com>
2023-11-26 11:46:06 +0700

    fix

--- src/main/java/Common/JdbcConfig.java
+++ src/main/java/Common/JdbcConfig.java
@@ -8,4 +8,5 @@ public class JdbcConfig {
     public static String JDBC_URL = "jdbc:sqlserver://DESKTOP-3UKTJM9\\SQLEXPRESS01:1433;databaseName=miniproject_english;TrustServerCertificate=true;";
     public static String USERNAME = "sa";
     public static String PASSWORD = "123456";
+
 }
