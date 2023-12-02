5512b327348494d4315939a3b4ee9edcf95cf0da
STM <stngocquynew@gmail.com>
2023-12-02 14:44:59 +0700

    -Khi thêm mới một record thì phải order by desc dòng đó lên đầu để người dùng biết dòng nào mới được thêm -Cột ID nên ẩn đi khi get list data thay vào đó ta đưa vào ArrayList để lấy index, khi người dùng nhập  index của List thì sẽ tự map với ID trong database, Các chức năng update, delete cũng vậy -Đối với chức năng update thì nên ra menu để người dùng update những cột nào, những cột quan trọng trong database thì nên giữ lại - Những cột liên quan đến khoá ngoại thì ta nên load TÊN lên giao diện

--- src/main/java/Model/DonateDetail.java
+++ src/main/java/Model/DonateDetail.java
@@ -4,18 +4,66 @@ import Common.DBConnect;
 
 import java.sql.Connection;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class DonateDetail {
     private Connection con = DBConnect.connectDatabase();
+    public ArrayList<DonateDetail> donateDetails = new ArrayList<>();
     private int id;
     private double amount;
     private LocalDate donateDate;
     private int commissionId;
     private int representativeId;
+
+    private String precintName; // New field
+    private String representativeName; // New field
+    private String companyName; // New field
+    private String officerName; // New field
+
+    // New constructor with additional fields
+    public DonateDetail(int id, double amount, LocalDate donateDate, int commissionId, int representativeId,
+                        String precintName, String representativeName, String companyName, String officerName) {
+        this.id = id;
+        this.amount = amount;
+        this.donateDate = donateDate;
+        this.commissionId = commissionId;
+        this.representativeId = representativeId;
+        this.precintName = precintName;
+        this.representativeName = representativeName;
+        this.companyName = companyName;
+        this.officerName = officerName;
+    }
+
+    // New getter methods for additional fields
+    public String getPrecintName() {
+        return precintName;
+    }
+
+    public String getRepresentativeName() {
+        return representativeName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getOfficerName() {
+        return officerName;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
     // Constructor
     public DonateDetail(){
     }
     public DonateDetail(Connection con) {
+
         this.con = con;
     }
     public DonateDetail(int id, double amount, LocalDate donateDate, int commissionId, int representativeId) {
@@ -25,33 +73,33 @@ public class DonateDetail {
         this.commissionId = commissionId;
         this.representativeId = representativeId;
     }
-    //Getter and Setter
-
     public int getId() {
+
         return id;
     }
 
     public void setId(int id) {
+
         this.id = id;
     }
-
     public double getAmount() {
         return amount;
     }
 
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
+
 
     public LocalDate getDonateDate() {
+
         return donateDate;
     }
 
     public void setDonateDate(LocalDate donateDate) {
+
         this.donateDate = donateDate;
     }
 
     public int getCommissionId() {
+
         return commissionId;
     }
 
@@ -66,7 +114,6 @@ public class DonateDetail {
     public void setRepresentativeId(int representativeId) {
         this.representativeId = representativeId;
     }
-
     @Override
     public String toString() {
         return "DonateDetail{" +
