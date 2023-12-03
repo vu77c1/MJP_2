8edca33a711cd19ccac2000d3052f0a4eb185dc7
hstm0ichi <113711220+hstm0ichi@users.noreply.github.com>
2023-12-04 06:39:54 +0700

    Fix lỗi tương thích

--- src/main/java/Model/DonateDetail.java
+++ src/main/java/Model/DonateDetail.java
@@ -4,25 +4,20 @@ import Common.DBConnect;
 
 import java.sql.Connection;
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 public class DonateDetail {
     private Connection con = DBConnect.connectDatabase();
-    public ArrayList<DonateDetail> donateDetails = new ArrayList<>();
     private int id;
     private double amount;
     private LocalDate donateDate;
     private int commissionId;
     private int representativeId;
+    private String precintName;
+    private String representativeName;
+    private String companyName;
+    private String officerName;
 
-    private String precintName; // New field
-    private String representativeName; // New field
-    private String companyName; // New field
-    private String officerName; // New field
-
-    // New constructor with additional fields
-    public DonateDetail(int id, double amount, LocalDate donateDate, int commissionId, int representativeId,
-                        String precintName, String representativeName, String companyName, String officerName) {
+    public DonateDetail(int id, double amount, LocalDate donateDate, int commissionId, int representativeId, String precintName, String representativeName, String companyName, String officerName) {
         this.id = id;
         this.amount = amount;
         this.donateDate = donateDate;
@@ -34,36 +29,50 @@ public class DonateDetail {
         this.officerName = officerName;
     }
 
-    // New getter methods for additional fields
-    public String getPrecintName() {
-        return precintName;
-    }
-
-    public String getRepresentativeName() {
-        return representativeName;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public String getOfficerName() {
-        return officerName;
+    public Connection getCon() {
+        return con;
     }
 
     public void setCon(Connection con) {
         this.con = con;
     }
 
+    public String getPrecintName() {
+        return precintName;
+    }
+
+    public void setPrecintName(String precintName) {
+        this.precintName = precintName;
+    }
+
+    public String getRepresentativeName() {
+        return representativeName;
+    }
+
+    public void setRepresentativeName(String representativeName) {
+        this.representativeName = representativeName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
+    }
+
+    public String getOfficerName() {
+        return officerName;
+    }
+
+    public void setOfficerName(String officerName) {
+        this.officerName = officerName;
     }
 
     // Constructor
     public DonateDetail(){
     }
     public DonateDetail(Connection con) {
-
         this.con = con;
     }
     public DonateDetail(int id, double amount, LocalDate donateDate, int commissionId, int representativeId) {
@@ -73,33 +82,33 @@ public class DonateDetail {
         this.commissionId = commissionId;
         this.representativeId = representativeId;
     }
-    public int getId() {
+    //Getter and Setter
 
+    public int getId() {
         return id;
     }
 
     public void setId(int id) {
-
         this.id = id;
     }
+
     public double getAmount() {
         return amount;
     }
 
-
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 
     public LocalDate getDonateDate() {
-
         return donateDate;
     }
 
     public void setDonateDate(LocalDate donateDate) {
-
         this.donateDate = donateDate;
     }
 
     public int getCommissionId() {
-
         return commissionId;
     }
 
@@ -114,14 +123,5 @@ public class DonateDetail {
     public void setRepresentativeId(int representativeId) {
         this.representativeId = representativeId;
     }
-    @Override
-    public String toString() {
-        return "DonateDetail{" +
-                "id=" + id +
-                ", amount=" + amount +
-                ", donateDate=" + donateDate +
-                ", commissionId=" + commissionId +
-                ", representativeId=" + representativeId +
-                '}';
-    }
+
 }
